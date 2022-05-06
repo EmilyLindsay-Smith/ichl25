6ab7cc8eff3732bf6f112e9535919f1872756b55
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-06 08:18:37 +0100

    creating tabbed structure for programme

--- src/components/Programme.jsx
+++ src/components/Programme.jsx
@@ -4,7 +4,36 @@ function Programme() {
     return (
         <div className="programme">
             <div class="container">
-                <h1> The Programme! </h1>
+                <h1> The Programme </h1>
+
+                <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link " id="nav-monday-tab" data-toggle="tab" href="#nav-monday" role="tab" aria-controls="nav-monday" aria-selected="true">Monday</a>
+                        <a class="nav-item nav-link " id="nav-tuesday-tab" data-toggle="tab" href="#nav-tuesday" role="tab" aria-controls="nav-tuesday" aria-selected="false">Tuesday</a>
+                        <a class="nav-item nav-link " id="nav-wednesday-tab" data-toggle="tab" href="#nav-wednesday" role="tab" aria-controls="nav-wednesday" aria-selected="false">Wednesday</a>
+                        <a class="nav-item nav-link " id="nav-thursday-tab" data-toggle="tab" href="#nav-thursday" role="tab" aria-controls="nav-thursday" aria-selected="false">Thursday</a>
+                        <a class="nav-item nav-link " id="nav-friday-tab" data-toggle="tab" href="#nav-friday" role="tab" aria-controls="nav-friday" aria-selected="false">Friday</a>
+                    </div>
+                </nav>
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane active" id="nav-monday" role="tabpanel" aria-labelledby="nav-monday-tab">
+                        Here is Monday's content 
+                    </div>
+                    <div class="tab-pane " id="nav-tuesday" role="tabpanel" aria-labelledby="nav-tuesday-tab">
+                        Here is Tuesday's content 
+                    </div>
+                    <div class="tab-pane " id="nav-wednesday" role="tabpanel" aria-labelledby="nav-wednesday-tab">
+                        Here is Wednesday's content 
+                    </div>
+                    <div class="tab-pane " id="nav-thursday" role="tabpanel" aria-labelledby="nav-thursday-tab">
+                        Here is Thursday's content 
+                    </div>
+                    <div class="tab-pane " id="nav-friday" role="tabpanel" aria-labelledby="nav-friday-tab">
+                        Here is Friday's content 
+                    </div>
+                </div>
+            
+  
             </div>
         </div>
     );
