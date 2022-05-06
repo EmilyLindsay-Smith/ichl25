d7230991d470bbc17cc96ab9eb15c89cfd6ded70
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-06 08:28:26 +0100

    add start of accordion for workshops

--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -5,6 +5,46 @@ function Workshops() {
         <div className="workshops">
             <div class="container">
                 <h1> The Workshops! </h1>
+
+                <div class="accordion" id="workshopAccordion">
+                    <div class="card">
+                        <div class="card-header" id="workshop1Header">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#workshopOne" aria-expanded="true" aria-controls="workshopOne">
+                                    Workshop 1
+                                </button>
+                            </h5>
+                        </div>
+                    <div id="workshopOne" class="collapse show" aria-labelledby="workshop1Header" data-parent="#workshopAccordion">
+                        <div class="card-body">
+                            <strong>Organisers:</strong> <br/> Organiser One <br/>
+                            <strong>Abstract:</strong><br/>
+                            WOrkshop ONe Abstract<br/>
+                            <strong>Timetable:</strong><br/>
+                            WOrkshop One timetable
+                        </div>
+                    </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="workshop2Header">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#workshopTwo" aria-expanded="false" aria-controls="workshopTwo">
+                                    Workshop 2
+                                </button>
+                            </h5>
+                        </div>
+                    <div id="workshopTwo" class="collapse show" aria-labelledby="workshop2Header" data-parent="#workshopAccordion">
+                        <div class="card-body">
+                            <strong>Organisers:</strong> <br/> Organiser One <br/>
+                            <strong>Abstract:</strong><br/>
+                            WOrkshop Two Abstract<br/>
+                            <strong>Timetable:</strong><br/>
+                            WOrkshop Two timetable
+                        </div>
+                    </div>
+                    </div>
+                </div> {/*acordiondiv*/}
+
             </div>
         </div>
     );
