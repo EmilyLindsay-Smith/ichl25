38c7372fe7b9345e2117e3bfffb7e222bbbed910
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-10 10:23:30 +0100

    initial filling out of correct data

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,3 +1,10 @@
 export const data = [
-    {id: 2, type: 'talk', day: 'monday', time: '0900', room: 'B', author: ['author1', 'author2'], title: 'a really cool title'},{id: 3, type: 'talk', day: 'monday', time: '1000', room: 'C', author: ['author6', 'author4'], title: 'a different title'}, {id: 4, type: 'talk', day: 'tuesday', time: '1000', room: 'B', author: ['Katalyn E. Kiss Pretty', 'author6'], title: 'a very different title'},{id: 5, type: 'talk', day: 'tuesday', time: '0900', room: 'C', author: ['author7', 'author8'], title: 'yet another very different title'}
+    {id: 90, type: 'talk', day: 'monday', time: '1130', room: 'L1', author: ['Laura Arnold'], title: 'The multiple causes of Split Inalienable Coding in northwest New Guinea: Contact meets drift', workshop:'', link:''},
+    {id: 200, type: 'talk', day: 'monday', time: '1130', room: 'L2', author: ['Jack Hoeksema'], title: 'Approximative adverbs in modern and premodern languages', workshop:'', link:''},
+    {id: 205, type: 'talk', day: 'monday', time: '1130', room: 'L3', author: ['Patrick Honeybone'], title: 'Should fortition have a place in diachronic phonological typology?', workshop:'', link:''},
+    {id: 270, type: 'talk', day: 'monday', time: '1130', room: 'L6', author: ['Michela Cennamo'], title: 'Paths of change in the diachrony of anti causatives in Romance: the rise and fall of lability', workshop:'', link:''},
+    {id: 229, type: 'talk', day: 'monday', time: '1130', room: 'C1', author: ['Stefan Hartmann', 'Lena Schnee'], title: 'Disentangling the constructional semantics of German future constructions', workshop:'', link:''},
+    {id: 11, type: 'talk', day: 'monday', time: '1130', room: 'C4', author: ['Ilja Seržant'], title: 'Cyclic changes in verbal person-number indexes are unlikely', workshop:'', link:''},
+    {id: 106, type: 'talk', day: 'monday', time: '1130', room: 'C6', author: ['Lena Kaltenbach','Carola Trips'], title: 'Language contact through translation as priming in historical data', workshop:'', link:''},
+
 ]
