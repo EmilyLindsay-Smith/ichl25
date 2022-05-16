e031285c19fca3822f1f186e79e9fae1df1880b6
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 17:06:44 +0100

    fix italics

--- src/components/PlenarySpeakers.jsx
+++ src/components/PlenarySpeakers.jsx
@@ -9,8 +9,8 @@ function PlenarySpeakers(){
             Previously she was a lecturer at the University of Salford and has also taught at universities including Oxford and (through Erasmus) Zurich. 
             Her main research interests are in historical and cognitive semantics and lexicology.
              She is particularly interested in the intra- and extra-linguistic factors in semantic change, and in tracing polysemy in relation to social-cultural change. 
-             Her monograph <emph>Metaphor and Metonymy: A Diachronic Approach </emph>, which uses data from the <emph>Historical Thesaurus of English</emph>, explores the motivation for metaphor and metonymy in the semantic field intelligence. 
-             She is also the co-author of <emph>English Historical Semantics</emph> (with Christian Kay), and has co-edited volumes on <emph>Historical Cognitive Linguistics</emph> and <emph>Current Methods in Historical Semantics </emph> for Mouton. 
+             Her monograph <em>Metaphor and Metonymy: A Diachronic Approach </em>, which uses data from the <em>Historical Thesaurus of English</em>, explores the motivation for metaphor and metonymy in the semantic field intelligence. 
+             She is also the co-author of <em>English Historical Semantics</em> (with Christian Kay), and has co-edited volumes on <em>Historical Cognitive Linguistics</em> and <em>Current Methods in Historical Semantics </em> for Mouton. 
              She is a member of the Keywords Project.
         </p>
         </div>
