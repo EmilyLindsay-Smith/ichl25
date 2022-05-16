e2ef6d487ebcbc643d2ccc90a5fb807862828949
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 16:19:42 +0100

    registration and fee info

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 
 function Registration(){
     return(
-        <div class="container container-pad"><h1> Registration and Fees </h1></div>
+        <div class="container container-pad"><h1> Registration and Fees </h1>
+        
+        <p><em>Note Early bird registration is no longer available</em></p>
+
+        <table class="table">
+            <tbody>
+            <tr>
+                <th scope="row">Standard </th>
+                <td>£340</td>
+            </tr>
+            <tr>
+                <th scope="row">Standard (without lunch) </th>
+                <td>£340</td>
+            </tr>
+            <tr>
+                <th scope="row">Student (with or without lunch)</th>
+                <td>£180</td>
+            </tr>
+
+            <tr>
+                <th scope="row">Conference Dinner only (partners) </th>
+                <td>£38.50</td>
+            </tr>
+            </tbody>
+        </table>
+        
+        <p>Booking closes on <strong>26th July 2022</strong></p>
+        <a href="https://www.oxforduniversitystores.co.uk/conferences-and-events/linguistics-philology-phonetics/events/ichl-2022" rel="noreferrer noopener" target="_blank">Click here to book via the Oxford University Stores website</a>
+        </div>
     );
 }
 
