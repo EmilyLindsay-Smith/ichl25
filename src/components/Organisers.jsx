1c1b2f581749b4dce3a378682c287a740321d4e8
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 13:22:31 +0100

    organsing committee - needs work

--- src/components/Organisers.jsx
+++ src/components/Organisers.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 
 function Organisers(){
     return(
-        <h1>Contact Us</h1>
+        <div>
+        <h1>Organisers</h1>
+        <h2>Organisers</h2>
+        <ul>
+            <li> Professor Aditi Lahiri</li>
+            <li> Professor Martin Maiden</li>
+        </ul>
+        <h2> Local Organising Committee</h2>
+        <ul>
+            <li> Dr. Holly Kennard</li>
+            <li>Dr. Emily Lindsay-Smith </li>
+        </ul>
+        <h2>Advisory Committee</h2>
+        <ul>
+            <li>oxford based reviewers</li>
+        </ul>
+        </div>
     );
 }
 
