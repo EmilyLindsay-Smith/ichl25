25877b510e32997540e597af1e9e71ca4cb64831
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 13:20:23 +0100

    current excursions content - but this needs editing

--- src/components/Excursions.jsx
+++ src/components/Excursions.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 
 function Excursions(){
     return(
-        <h1>Contact Us</h1>
+        <div>
+        <h1>Excursions and Social Events</h1>
+       
+<p>We hope you are looking forward to ICHL this summer. There are many wonderful places to visit within driving distance of Oxford, and we intend to book coaches for trips to explore some of them on Saturday 6th August. However, we realise that many of you might want to leave early, or to spend the day in Oxford; there will be events laid on in Oxford, and there will also be opportunities for excursions and visits on Wednesday afternoon – more details on both of these to follow.</p>
+
+In order to make it affordable, could we ask you to express an interest in one of the following options by 31st May 2022 via this form: https://forms.gle/W9hJHiiV7mYfJAT5A
+
+We will organise coaches based on the level of interest, then open booking to you for these trips.
+
+Basildon Park https://www.nationaltrust.org.uk/basildon-park
+
+Basildon Park features a historic house and gardens and was used extensively during the filming of the Netflix series Bridgerton.
+
+ 
+
+Lacock Abbey, Fox Talbot Museum https://www.nationaltrust.org.uk/lacock-abbey-fox-talbot-museum-and-village
+
+A visit to Lacock Abbey features the Fox Talbot Museum, dedicated to one of the pioneers of early photography.
+
+ 
+
+Mappa Mundi at Hereford Cathedral https://www.themappamundi.co.uk/
+
+The Mappa Mundi at Hereford Cathedral dates from the early 14th Century. The Cathedral also features a historic library with texts dating back to the early 9th Century.
+
+ 
+
+Bletchley Park: https://bletchleypark.org.uk/
+
+Bletchley Park was home to Alan Turing's codebreaking team that decoded the Enigma Machine during World War II.
+
+ 
+
+The entrance cost for each location is available on their websites. The National Trust properties are accessible without charge to members of the National Trust as well as members from other organisations abroad. Please consult their website https://www.nationaltrust.org.uk/features/overseas-organisations for more information.
+</div>
     );
 }
 
