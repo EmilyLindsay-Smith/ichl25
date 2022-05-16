5548c7626be590798f9b94df4383bf849882cc94
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 13:18:08 +0100

    accommodation page filled

--- src/components/Accommodation.jsx
+++ src/components/Accommodation.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Accommodation(){
     return(
-        <h1>Contact Us</h1>
+        <div>
+        <h1>Accommodation</h1>
+       <p>Accommodation is available at <a href="https://www.some.ox.ac.uk/" rel=" noopener noreferrer" target="_blank">Somerville College</a>, just a couple of minutes' walk from the Mathematical Institute. 
+        Bookings can be made up until 5pm on Friday 15th July.</p>
+
+        <p>To make a booking, please click <a href="https://www.universityrooms.com/en-GB/eventcode?ec=ICHC2022&vid=somervilleoxford" rel=" noopener noreferrer" target="_blank">here</a>.</p>
+        </div>
     );
 }
 
