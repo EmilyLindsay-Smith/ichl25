91a1e34fb41e45484b9cfcac426b87b4403c27b8
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 13:32:36 +0100

    Kathryn Allan content

--- src/components/PlenarySpeakers.jsx
+++ src/components/PlenarySpeakers.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 function PlenarySpeakers(){
     return(
-        <h1>Contact Us</h1>
+        <div class="container container-pad">
+            <h1> Plenary Speakers </h1>
+        <h4><a href="https://www.ucl.ac.uk/english/people/kathryn-allan" target="_blank" rel="noreferrer">Kathryn Allan (University College London) </a></h4>
+        <p>Kathryn Allan is an Associate Professor in the History of English at University College, London. 
+            Previously she was a lecturer at the University of Salford and has also taught at universities including Oxford and (through Erasmus) Zurich. 
+            Her main research interests are in historical and cognitive semantics and lexicology.
+             She is particularly interested in the intra- and extra-linguistic factors in semantic change, and in tracing polysemy in relation to social-cultural change. 
+             Her monograph <emph>Metaphor and Metonymy: A Diachronic Approach </emph>, which uses data from the <emph>Historical Thesaurus of English</emph>, explores the motivation for metaphor and metonymy in the semantic field intelligence. 
+             She is also the co-author of <emph>English Historical Semantics</emph> (with Christian Kay), and has co-edited volumes on <emph>Historical Cognitive Linguistics</emph> and <emph>Current Methods in Historical Semantics </emph> for Mouton. 
+             She is a member of the Keywords Project.
+        </p>
+        </div>
     );
 }
 
-export default PlenarySpeakers
+export default PlenarySpeakers;
