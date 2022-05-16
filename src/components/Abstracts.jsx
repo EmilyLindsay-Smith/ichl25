ed7135cac2cb092e461995d0aa1d9482f122c286
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 17:42:08 +0100

    sorting size of pdf

--- src/components/Abstracts.jsx
+++ src/components/Abstracts.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Abstracts(){
 
     return(
-        <div class="container container-pad">
+        <div class="container container-pad max-view">
             Abstracts
         <object data={require('./presentation-abstracts/ICHL25_paper_2.pdf')} type="application/pdf" width="100%" height="150%">
             <p>Alternative text - include a link <a href={require('./presentation-abstracts/ICHL25_paper_2.pdf')}>to the PDF!</a></p>
