470769831164f01287a3a1490435d286ecbba5a7
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-05-16 16:18:39 +0100

    sorting out the excursions page

--- src/components/Excursions.jsx
+++ src/components/Excursions.jsx
@@ -5,37 +5,50 @@ function Excursions(){
         <div class="container container-pad">
         <h1>Excursions and Social Events</h1>
        
-<p>We hope you are looking forward to ICHL this summer. There are many wonderful places to visit within driving distance of Oxford, and we intend to book coaches for trips to explore some of them on Saturday 6th August. However, we realise that many of you might want to leave early, or to spend the day in Oxford; there will be events laid on in Oxford, and there will also be opportunities for excursions and visits on Wednesday afternoon – more details on both of these to follow.</p>
+    <p>We hope you are looking forward to ICHL this summer. There are many wonderful places to visit within driving distance of Oxford, not to mention a wide variety of options within the city itself </p>
 
-In order to make it affordable, could we ask you to express an interest in one of the following options by 31st May 2022 via this form: https://forms.gle/W9hJHiiV7mYfJAT5A
+    <h4>Organised Wednesday Afternoon Trip</h4>
+        <p>There is plenty to do within Oxford city itself.</p>
+        <p> If there is sufficient interest, we will organise a coach for a trip to Blenheim Palace - more details to follow soon. </p>
 
-We will organise coaches based on the level of interest, then open booking to you for these trips.
+    <h4>Organised Saturday Trips</h4>
+        <p> If there is sufficient interest, we will organise a coach for a trip to <a href="https://www.themappamundi" target="_blank" rel="noreferrer noopener">Mappa Mundi at Hereford Cathedral</a> and <a href="https://bletchleypark.org.uk/" target="_blank" rel="noreferrer noopener">Bletchley Park</a> - more details to follow soon. </p>
+        <p>The Mappa Mundi at Hereford Cathedral dates from the early 14th Century. The Cathedral also features a historic library with texts dating back to the early 9th Century.</p>
+        <p>Bletchley Park was home to Alan Turing's codebreaking team that decoded the Enigma Machine during World War II. </p>
 
-Basildon Park https://www.nationaltrust.org.uk/basildon-park
+   <h4> Suggestions of Places to Visit </h4>
+    <h5> Within Oxford </h5>
+    To Visit:
+    <ul>
+        <li>Oxford University Museum of Natural History</li>
+        <li>Pitt Rivers Museum</li>
+        <li>Ashmolean Museum of Art and Archaeology</li>
+        <li>Bodleian Library</li>
+        <li>Modern Art Oxford</li>
+        <li>History of Science Museum</li> 
+    </ul>
+    To Wander:
+    <ul>
+        <li>Oxford Botanic Garden</li>
+        <li> ChristChurch Meadows</li>
+        <li> University Parks</li> 
+        <li> Port Meadow</li>
+        <li> Iffley Lock </li>
+        <li> Oxford Canal </li>
+        <li> Shotover Country Park</li>
+    </ul>
+    <h5> Within Driving Distance </h5>
+        <p><strong><a href="https://www.nationaltrust.org.uk/basildon-park" target="_blank" rel="noreferrer noopener">Basildon Park</a></strong>
+        <br/>
+        Basildon Park features a historic house and gardens and was used extensively during the filming of the Netflix series Bridgerton.</p>
 
-Basildon Park features a historic house and gardens and was used extensively during the filming of the Netflix series Bridgerton.
+        <p><strong><a href="https://www.nationaltrust.org.uk/lacock-abbey-fox-talbot-museum-and-village" target="_blank" rel="noreferrer noopener">Lacock Abbey, Fox Talbot Museum</a></strong>
+        <br/>
+        A visit to Lacock Abbey features the Fox Talbot Museum, dedicated to one of the pioneers of early photography.
+        </p>
 
- 
-
-Lacock Abbey, Fox Talbot Museum https://www.nationaltrust.org.uk/lacock-abbey-fox-talbot-museum-and-village
-
-A visit to Lacock Abbey features the Fox Talbot Museum, dedicated to one of the pioneers of early photography.
-
- 
-
-Mappa Mundi at Hereford Cathedral https://www.themappamundi.co.uk/
-
-The Mappa Mundi at Hereford Cathedral dates from the early 14th Century. The Cathedral also features a historic library with texts dating back to the early 9th Century.
-
- 
-
-Bletchley Park: https://bletchleypark.org.uk/
-
-Bletchley Park was home to Alan Turing's codebreaking team that decoded the Enigma Machine during World War II.
-
- 
-
-The entrance cost for each location is available on their websites. The National Trust properties are accessible without charge to members of the National Trust as well as members from other organisations abroad. Please consult their website https://www.nationaltrust.org.uk/features/overseas-organisations for more information.
+        <p><strong><a href="">University of Oxford Harcourt Arboretum </a></strong></p>
+<p>The entrance cost for each location is available on their websites. The National Trust properties are accessible without charge to members of the National Trust as well as members from other organisations abroad. Please consult their <a href="https://www.nationaltrust.org.uk/features/overseas-organisations" target="_blank" rel="noreferrer noopener">website</a> for more information.</p>
 </div>
     );
 }
