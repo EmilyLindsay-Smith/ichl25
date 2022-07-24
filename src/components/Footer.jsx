c79039a6ac9c02d626aa4df203ef29abd0a0bae1
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-07-24 11:55:50 +0100

    removed hi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,12 @@ function Footer(){
 
     return (
         <div className="footer">
-            <p>hi</p>
             <footer>
                 <div className="py-2 bg-ox fixed-bottom container fluid-containerB">
                     <div></div>
                     <div className="text-center text-white footer-email">
                         ICHL25 <br/>
-                        created by <span className="text-white"><a href="mailto:emily.lindsay-smith@ling-phil.ox.ac.uk">Dr. Emily Lindsay-Smith </a></span>
+                        created by <a href="mailto:emily.lindsay-smith@ling-phil.ox.ac.uk">Dr. Emily Lindsay-Smith </a>
                     </div>
                     <div className="text-center">
                         <a href="#navigationjump" className="topreturn">Return to top</a>
