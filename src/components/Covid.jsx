7fa0c443f4e164a7dbc8cd49a3af8ce905569d08
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2022-07-26 15:49:45 +0100

    add

--- src/components/Covid.jsx
+++ src/components/Covid.jsx
@@ -5,10 +5,10 @@ export default function Covid() {
         <div>
         <div className="container container-pad">
     <h1 id="site-top"> Covid Policy</h1>
-    <p>Covid has unfortunately not disappeared and for everyone’s safety we need to continue to follow some safety measures.</p>
+    <p>COVID has unfortunately not disappeared and for everyone’s safety we need to continue to follow some safety measures.</p>
     <ul>
     <li>With regard to the rising infection rate of COVID, we advise participants to do a COVID (lateral flow) test before attending each morning, even if you do not feel symptomatic.</li>
-    <li>Please do not attend if you are positive or displaying symptoms of Covid-19 and believe you may be infectious to others, to avoid transmitting COVID to the other attendees.</li>
+    <li>Please do not attend if you are positive or displaying symptoms of COVID-19 and believe you may be infectious to others, to avoid transmitting COVID to the other attendees.</li>
     <li>We encourage all attendees to wear masks, and will provide hand sanitiser at the entrance as well as some masks.</li>
     </ul>
     <p>We realise that you will need to take your masks off to eat and drink, but there are plenty of beautiful spaces outside in the Radcliffe Observatory Quarter, and several seating areas where you can take refreshments.</p>
